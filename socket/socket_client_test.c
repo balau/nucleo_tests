a78d89e2309690cf717231cf3da102fafd2a740b
Francesco Balducci <balau@users.sourceforge.net>
2015-10-17 20:16:59 +0200

    Simple TCP client test

--- socket/socket_client_test.c
+++ socket/socket_client_test.c
@@ -1,30 +1,68 @@
 /*
- * Copyright (c) 2015 Francesco Balducci
- *
- * This file is part of nucleo_tests.
- *
- *    nucleo_tests is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    nucleo_tests is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with nucleo_tests.  If not, see <http://www.gnu.org/licenses/>.
- */
-#include "socket.h"
-
-int main(void)
+    C ECHO client example using sockets
+    http://www.binarytides.com/server-client-example-c-sockets-linux/
+*/
+#include <stdio.h> //printf
+#include <string.h>    //strlen
+#include "socket.h" //<sys/socket.h>    //socket
+#include "inet.h" //<arpa/inet.h> //inet_addr
+ 
+int main(int argc , char *argv[])
 {
-    int fdsocket;
+    int sock;
+    struct sockaddr_in server;
+    char message[1000] , server_reply[2000];
+     
+    (void)argc;
+    (void)argv;
 
-    fdsocket = socket(AF_INET, SOCK_STREAM, 0);
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    //server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_addr.s_addr = inet_addr("192.168.1.173");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
+ 
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+     
+    puts("Connected\n");
+     
+    //keep communicating with server
     while(1)
     {
+        printf("Enter message : ");
+        scanf("%s" , message);
+         
+        //Send some data
+        if( send(sock , message , strlen(message) , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+         
+        //Receive a reply from the server
+        if( recv(sock , server_reply , 2000 , 0) < 0)
+        {
+            puts("recv failed");
+            break;
+        }
+         
+        puts("Server reply :");
+        puts(server_reply);
     }
+     
+    close(sock);
+    return 0;
 }
 
