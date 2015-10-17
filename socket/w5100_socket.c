f19eb1a6b0dd5562bb3b2e5c57b15c2a5a53767d
Francesco Balducci <balau@users.sourceforge.net>
2015-10-17 20:16:31 +0200

    Add more functionality to init and connect

--- socket/w5100_socket.c
+++ socket/w5100_socket.c
@@ -20,14 +20,27 @@
 /* #include <sys/socket.h> */
 #include "socket.h"
 #include "w5100.h"
+#include "inet.h" // <arpa/inet.h>
 
 #define W5100_SOCKET_FREE (-1)
+
+#ifndef W5100_IP_ADDR
+#  define W5100_IP_ADDR "192.168.1.100"
+#endif
+#ifndef W5100_SUBNET
+#  define W5100_SUBNET "255.255.255.0"
+#endif
+#ifndef W5100_GATEWAY_ADDR
+#  define W5100_GATEWAY_ADDR "192.168.1.1"
+#endif
 
 void w5100_socket_init(void);
 
 static struct w5100_socket {
     int fd;
 } w5100_sockets[W5100_N_SOCKETS];
+
+static uint8_t w5100_mac_addr[6] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85};
 
 static
 int isocket_to_fd(int isocket)
@@ -125,19 +138,103 @@ int socket(int domain, int type, int protocol)
     return ret;
 }
 
+int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
+{
+    int ret;
+    int isocket;
+
+    isocket = isocket_to_fd(sockfd);
+
+    if ( addr->sa_family != AF_INET )
+    {
+        errno = EAFNOSUPPORT;
+        ret = -1;
+    }
+    else /* TODO: UPD and RAW */
+    {
+        struct sockaddr_in *server;
+        uint8_t sr;
+
+        (void)addrlen;
+
+        server = (struct sockaddr_in *)addr;
+        /* TODO: check if already in use EADDRINUSE */
+        /* TODO: check EBADF */
+        /* TODO: check ENOTSOCK */
+        w5100_write_sock_reg(W5100_Sn_MR, isocket, 0x01); /* TCP */
+        w5100_write_sock_regx(W5100_Sn_PORT, isocket, &server->sin_port); /* Needed? */
+        w5100_write_sock_reg(W5100_Sn_CR, isocket, 0x01); /* OPEN */
+        w5100_write_sock_regx(W5100_Sn_DIPR, isocket, &server->sin_addr.s_addr);
+        w5100_write_sock_regx(W5100_Sn_DPORT, isocket, &server->sin_port);
+        w5100_write_sock_reg(W5100_Sn_CR, isocket, 0x04); /* CONNECT */
+        do {
+            sr = w5100_read_sock_reg(W5100_Sn_SR, isocket);
+        } while ((sr == 0x01) || (sr == 0x15)); /* INIT or SYNSENT */
+        if (sr == 0x17) /* ESTABLISHED */
+        {
+            ret = 0;
+        }
+        else if (sr == 0x00)
+        {
+            errno = ETIMEDOUT;
+            ret = -1;
+        }
+        else
+        {
+            errno = ETIMEDOUT; /* TODO: better error? */
+            ret = -1;
+        }
+    }
+    return ret;
+}
+
+ssize_t recv(int sockfd, void *buf, size_t len, int flags)
+{
+    (void)sockfd;
+    (void)buf;
+    (void)len;
+    (void)flags;
+    /* TODO */
+    errno = ENOTCONN;
+    return -1;
+}
+
+ssize_t send(int sockfd, const void *buf, size_t len, int flags)
+{
+    (void)sockfd;
+    (void)buf;
+    (void)len;
+    (void)flags;
+    /* TODO */
+    errno = ENOTCONN;
+    return -1;
+}
 
 __attribute__((__constructor__))
 void w5100_socket_init(void)
 {
     int i;
+    in_addr_t addr;
 
     w5100_init();
 
+    w5100_write_reg(W5100_MR, 0x80); /* RST */
+    while(w5100_read_reg(W5100_MR) & 0x80) /* RST bit clears by itself */
+    {
+        continue;
+    }
+    
     for (i = 0; i < W5100_N_SOCKETS; i++)
     {
         w5100_sockets[i].fd = W5100_SOCKET_FREE;
     }
     w5100_write_reg(W5100_RMSR, 0x55); /* 2KiB per socket */
-    
+    w5100_write_regx(W5100_SHAR, w5100_mac_addr);
+    addr = inet_addr(W5100_IP_ADDR);
+    w5100_write_regx(W5100_SIPR, &addr);
+    addr = inet_addr(W5100_GATEWAY_ADDR);
+    w5100_write_regx(W5100_GAR, &addr);
+    addr = inet_addr(W5100_SUBNET);
+    w5100_write_regx(W5100_SUBR, &addr);
 }
 
