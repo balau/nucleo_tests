c9901b52101d0bfa498183972c9d8ddb02b0d6dd
Francesco Balducci <balau@users.sourceforge.net>
2015-10-28 22:16:17 +0100

    Added defines and comments

--- include/sys/socket.h
+++ include/sys/socket.h
@@ -40,22 +40,48 @@ struct sockaddr_storage
     char sa_data[6];
 };
 
-#define AF_INET 0x1
-#define AF_INET6 0x2
-#define AF_UNIX 0x3
-#define AF_UNSPEC 0x4
+#define AF_INET   0x1 /* Internet domain sockets for use with IPv4 addresses. */
+#define AF_INET6  0x2 /* Internet domain sockets for use with IPv6 addresses. */
+#define AF_UNIX   0x3 /* UNIX domain sockets. */
+#define AF_UNSPEC 0x4 /* Unspecified. */
 
-#define SOCK_STREAM    0x1
-#define SOCK_DGRAM     0x2
-#define SOCK_RAW       0x3
-#define SOCK_SEQPACKET 0x4
-#define SOCK_NONBLOCK 0x8
+#define SOCK_STREAM    0x1 /* Byte-stream socket. */
+#define SOCK_DGRAM     0x2 /* Datagram socket. */
+#define SOCK_RAW       0x3 /* Raw Protocol Interface. */
+#define SOCK_SEQPACKET 0x4 /* Sequenced-packet socket. */
 
-#define SOMAXCONN 1
+#define MSG_CTRUNC    0x01 /* Control data truncated. */
+#define MSG_DONTROUTE 0x02 /* Send without using routing tables. */
+#define MSG_EOR       0x04 /* Terminates a record (if supported by the protocol). */
+#define MSG_OOB       0x08 /* Out-of-band data. */
+#define MSG_PEEK      0x10 /* Leave received data in queue. */
+#define MSG_TRUNC     0x20 /* Normal data truncated. */
+#define MSG_WAITALL   0x40 /* Attempt to fill the read buffer. */
 
-#define SHUT_RD   0x1
-#define SHUT_WR   0x2
-#define SHUT_RDWR 0x3
+#define SO_ACCEPTCONN 0x00 /* Socket is accepting connections. */
+#define SO_BROADCAST 0x00 /* Transmission of broadcast messages is supported. */
+#define SO_DEBUG 0x00 /* Debugging information is being recorded. */
+#define SO_DONTROUTE 0x00 /* Bypass normal routing. */
+#define SO_ERROR 0x00 /* Socket error status. */
+#define SO_KEEPALIVE 0x00 /* Connections are kept alive with periodic messages. */
+#define SO_LINGER 0x00 /* Socket lingers on close. */
+#define SO_OOBINLINE 0x00 /* Out-of-band data is transmitted in line. */
+#define SO_RCVBUF 0x00 /* Receive buffer size. */
+#define SO_RCVLOWAT 0x00 /* Receive ``low water mark''. */
+#define SO_RCVTIMEO 0x00 /* Receive timeout. */
+#define SO_REUSEADDR 0x00 /* Reuse of local addresses is supported. */
+#define SO_SNDBUF 0x00 /* Send buffer size. */
+#define SO_SNDLOWAT 0x00 /* Send ``low water mark''. */
+#define SO_SNDTIMEO 0x00 /* Send timeout. */
+#define SO_TYPE 0x00 /* Socket type. */
+
+#define SOL_SOCKET 0xFF /* Options to be accessed at socket level, not protocol level. */
+
+#define SOMAXCONN 1 /* The maximum backlog queue length. */
+
+#define SHUT_RD   0x1 /* Disables further receive operations. */
+#define SHUT_WR   0x2 /* Disables further send and receive operations. */
+#define SHUT_RDWR 0x3 /* Disables further send operations. */
 
 extern
 int     accept(int, struct sockaddr *__restrict, socklen_t *__restrict);
