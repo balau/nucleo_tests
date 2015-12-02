9876969b456fe9afcec6bb7e3b8d372d75b1a226
Francesco Balducci <balau@users.sourceforge.net>
2015-12-02 22:46:58 +0100

    Test for w5100_dhcp.c

---
+++ tests/w5100_dhcp/w5100_dhcp_test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <time.h>
+#include "w5100.h"
+#include "w5100_dhcp.h"
+
+static
+void print_ipaddr(const char *name, in_addr_t addr)
+{
+    struct in_addr in;
+
+    in.s_addr = addr;
+    printf("%s: %s\n", name, inet_ntoa(in));
+}
+
+static
+time_t next;
+
+static
+int loop(void)
+{
+    in_addr_t ip;
+
+    next = w5100_dhcp();
+    w5100_read_regx(W5100_SIPR, &ip);
+    print_ipaddr("IP", ip);
+    print_ipaddr("DNS", w5100_getdns());
+    sleep(5);
+
+    return 0;
+}
+
+int main(void)
+{
+    while(1)
+    {
+        loop();
+    }
+}
+
