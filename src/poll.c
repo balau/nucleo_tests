f6b934d19ef6a7b92198914cc9cacdb3c44a2162
Francesco Balducci <balau@users.sourceforge.net>
2015-12-12 21:54:54 +0100

    Add poll implementation.

--- src/poll.c
+++ src/poll.c
@@ -18,13 +18,132 @@
  */
 #include <poll.h>
 #include <errno.h>
+#include "file.h"
+#include "time.h"
+#include "timespec.h"
+
+static
+short poll_one(struct pollfd *p)
+{
+    short revents;
+
+    if (p->fd < 0)
+    {
+        revents = 0;
+    }
+    else
+    {
+        struct fd *f;
+
+        f = file_struct_get(p->fd);
+        if (f == NULL)
+        {
+            errno = EINVAL;
+            revents = -1;
+        }
+        else if (f->poll == NULL)
+        {
+            errno = EINVAL;
+            revents = -1;
+        }
+        else
+        {
+            revents = f->poll(p->fd);
+        }
+    }
+    if (revents >= 0)
+    {
+        p->revents = p->events & revents;
+    }
+
+    return revents;
+}
+
+static
+int poll_tentative(struct pollfd fds[], nfds_t nfds)
+{
+    int ret;
+    nfds_t i;
+    int nfiles_ready;
+
+    nfiles_ready = 0;
+
+    for (i = 0; i < nfds; i++)
+    {
+        short revents;
+
+        revents = poll_one(&fds[i]);
+        if (revents == -1)
+        {
+            break;
+        }
+        else if (revents > 0)
+        {
+            nfiles_ready++;
+        }
+    }
+    if (i == nfds)
+    {
+        ret = nfiles_ready;
+    }
+    else
+    {
+        ret = -1;
+    }
+
+    return ret;
+}
 
 int poll(struct pollfd fds[], nfds_t nfds, int timeout)
 {
     int ret;
 
-    errno = ENOSYS;
-    ret = -1;
+    if (timeout == 0)
+    {
+        ret = poll_tentative(fds, nfds);
+    }
+    else
+    {
+        struct timespec tend;
+        int timeout_expired;
+
+        if (timeout == -1)
+        {
+            tend = TIMESPEC_INFINITY;
+        }
+        else
+        {
+            struct timespec tcurrent;
+            struct timespec timeout_ts;
+
+            timeout_ts.tv_sec = timeout / MSECS_IN_SEC;
+            timeout -= timeout_ts.tv_sec * MSECS_IN_SEC;
+            timeout_ts.tv_nsec = timeout * (NSECS_IN_SEC / MSECS_IN_SEC);
+
+            ret = clock_gettime(CLOCK_MONOTONIC, &tcurrent);
+
+            timespec_add(&tcurrent, &timeout_ts, &tend);
+        }
+        do
+        {
+            struct timespec tcurrent;
+
+            ret = clock_gettime(CLOCK_MONOTONIC, &tcurrent);
+            if (ret != 0)
+            {
+                break;
+            }
+
+            ret = poll_tentative(fds, nfds);
+            if (ret != 0)
+            {
+                break;
+            }
+
+            timeout_expired = (timespec_diff(&tcurrent, &tend, NULL) > 0);
+        } while(!timeout_expired);
+
+    }
 
     return ret;
 }
