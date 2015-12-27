f7707dfc6304d2b835dd99a843d71e637265d2fe
Francesco Balducci <balau@users.sourceforge.net>
2015-12-27 11:25:23 +0100

    Add sys/uio.h POSIX header.

---
+++ include/sys/uio.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Francesco Balducci
+ *
+ * This file is part of nucleo_tests.
+ *
+ *    nucleo_tests is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    nucleo_tests is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with nucleo_tests.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef SYS_UIO_H
+#define SYS_UIO_H
+
+#include <sys/types.h>
+
+struct iovec {
+    void *iov_base;
+    size_t iov_len;
+};
+
+ssize_t readv(int, const struct iovec *, int);
+
+ssize_t writev(int, const struct iovec *, int);
+
+#endif /* SYS_UIO_H */
+
