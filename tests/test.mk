371d11f532cc78c3d710f2608c9805ec715a4416
Francesco Balducci <balau@users.sourceforge.net>
2016-02-02 20:12:03 +0100

    Use target-specific makefiles

--- tests/test.mk
+++ tests/test.mk
@@ -1,11 +1,12 @@
 
 ROOT_DIR = ../../
 OPENCM3_DIR = $(ROOT_DIR)/libopencm3
-LDSCRIPT = $(ROOT_DIR)/scripts/stm32f103rb.ld
-OOCD_INTERFACE = stlink-v2-1
-OOCD_BOARD = st_nucleo_f103rb
 
 CPPFLAGS += -I$(ROOT_DIR)/include
+include $(ROOT_DIR)/scripts/posix.mk
 
-include $(ROOT_DIR)/scripts/libopencm3.target.mk
+#include $(ROOT_DIR)/scripts/libopencm3.target.mk
+include $(ROOT_DIR)/scripts/libopencm3.stm32f103rb.mk
+#include $(ROOT_DIR)/scripts/libopencm3.stm32f411re.mk
+
 
