eb2cc1942618f745286028c3cc591012b92d56bc
Francesco Balducci <balau@users.sourceforge.net>
2016-02-03 22:44:46 +0100

    Verify certificate on handshake.

--- tests/mbedtls/mbedtls_test.c
+++ tests/mbedtls/mbedtls_test.c
@@ -57,6 +57,9 @@ void mbedtls_printerr(int err, const char *func)
     fprintf(stderr, "error: %s returned %d : %s", func, err, errstr );
 }
 
+extern unsigned char digicert_der[];
+extern unsigned int digicert_der_len;
+
 int main(void)
 {
     struct addrinfo hints;
@@ -65,6 +68,7 @@ int main(void)
     int sock;
     int res;
 
+    char server_name[] = "www.eff.org";
     char http_get[200] =
         "GET /index.html HTTP/1.1\r\n"
         "Host: www.eff.org\r\n"
@@ -74,13 +78,16 @@ int main(void)
 
     mbedtls_ssl_context ssl;
     mbedtls_ssl_config conf;
-    
+    mbedtls_x509_crt cacert;
+
     printf("Press any key to continue...");
     getchar();
     printf("\n");
 
     mbedtls_ssl_init( &ssl );
     mbedtls_ssl_config_init( &conf );
+    mbedtls_x509_crt_init( &cacert );
+
     res = mbedtls_ssl_config_defaults(
             &conf,
             MBEDTLS_SSL_IS_CLIENT,
@@ -91,7 +98,9 @@ int main(void)
         mbedtls_printerr(res, "mbedtls_ssl_config_defaults");
         return 1;
     }
+#if 0
     mbedtls_ssl_conf_authmode( &conf, MBEDTLS_SSL_VERIFY_NONE);
+#endif
     mbedtls_ssl_conf_ciphersuites( &conf, cipher_list);
 #if 0
     res = mbedtls_ssl_conf_max_frag_len( &conf, MBEDTLS_SSL_MAX_FRAG_LEN_512);
@@ -108,13 +117,26 @@ int main(void)
         mbedtls_printerr(res, "mbedtls_ssl_setup");
         return 1;
     }
+    res = mbedtls_x509_crt_parse(&cacert, digicert_der, digicert_der_len);
+    if( res != 0 )
+    {
+        mbedtls_printerr(res, "mbedtls_x509_crt_parse");
+        return 1;
+    }
+    mbedtls_ssl_conf_ca_chain( &conf, &cacert, NULL );
+    res = mbedtls_ssl_set_hostname( &ssl, server_name);
+    if( res != 0 )
+    {
+        mbedtls_printerr(res, "mbedtls_ssl_set_hostname");
+        return 1;
+    }
 
     hints.ai_flags = 0;
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = 0;
 
-    res = getaddrinfo("www.eff.org", NULL, &hints, &ai);
+    res = getaddrinfo(server_name, NULL, &hints, &ai);
     if (res != 0)
     {
         if (res == EAI_SYSTEM)
@@ -162,6 +184,26 @@ int main(void)
     /* TLS connect */
     mbedtls_ssl_set_bio( &ssl, &sock, wrap_send, wrap_recv, NULL);
     res = mbedtls_ssl_handshake( &ssl );
+    do
+    {
+        const mbedtls_x509_crt *peer_cert;
+
+        peer_cert = mbedtls_ssl_get_peer_cert(&ssl);
+        if (peer_cert == NULL)
+        {
+            fprintf(stderr, "no peer cert.\n");
+        }
+        else
+        {
+            int n;
+            char info_str[200];
+
+            n = mbedtls_x509_crt_info(info_str, sizeof(info_str), "", peer_cert);
+            fputs("Certificate:\n", stderr);
+            fputs(info_str, stderr);
+            fputs("\n", stderr);
+        }
+    } while(0);
     if (res != 0)
     {
         mbedtls_printerr(res, "mbedtls_ssl_handshake");
