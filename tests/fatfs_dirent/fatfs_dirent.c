8716b52a115a0fb43ede22d5a99d385e6b3f6c1f
Francesco Balducci <balau@users.sourceforge.net>
2016-06-03 12:02:26 +0200

    Add fatfs_dirent test

---
+++ tests/fatfs_dirent/fatfs_dirent.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016 Francesco Balducci
+ *
+ * This file is part of nucleo_tests.
+ *
+ *    nucleo_tests is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    nucleo_tests is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with nucleo_tests.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+static
+void wait_enter(void)
+{
+    int c;
+
+    do {
+        c = getchar();
+    } while ((c != '\n') && (c != '\r'));
+}
+
+int main(void)
+{
+    const char *dirpath = "dirtest";
+    int result;
+
+    printf(
+            "fatfs_dirent\n"
+            "Press Enter to continue...\n");
+    wait_enter();
+
+    result = mkdir(dirpath, S_IRWXU | S_IRWXG | S_IRWXO);
+    if (result != 0)
+    {
+        if (errno == EEXIST)
+        {
+            printf("Directory already exists. Continuing...\n");
+        }
+        else
+        {
+            perror(dirpath);
+            return 1;
+        }
+    }
+    printf("Done.\n");
+
+    return 0;
+}
+
