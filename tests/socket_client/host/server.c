68de1d2b984be2d04ddf6c820154ea78620b262f
Francesco Balducci <balau@users.sourceforge.net>
2018-07-14 17:04:59 +0200

    add host test for socket_client test

---
+++ tests/socket_client/host/server.c
@@ -0,0 +1,102 @@
+/*
+    C socket server example
+    http://www.binarytides.com/server-client-example-c-sockets-linux/
+*/
+ 
+#include <stdio.h>
+#include <string.h>    //strlen
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
+#include <unistd.h>    //write
+
+static
+int loop(void)
+{
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[2000];
+     
+    printf("Press any key to continue...");
+    getchar();
+    printf("\n");
+    
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        perror("Could not create socket");
+    }
+    puts("Socket created");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) != 0)
+    {
+        //print the error message
+        perror("bind failed.");
+        close(socket_desc);
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    if (listen(socket_desc , SOMAXCONN) != 0)
+    {
+        //print the error message
+        perror("listen failed.");
+        close(socket_desc);
+        return 1;
+    }
+    
+    while(1)
+    { 
+        //Accept and incoming connection
+        puts("Waiting for incoming connections...");
+        c = sizeof(struct sockaddr_in);
+         
+        //accept connection from an incoming client
+        client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+        if (client_sock < 0)
+        {
+            perror("accept failed.");
+            close(socket_desc);
+            return 1;
+        }
+        puts("Connection accepted");
+         
+        //Receive a message from client
+        while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
+        {
+            printf("recv: %s\n", client_message);
+            //Send the message back to client
+            write(client_sock , client_message , strlen(client_message)+1);
+        }
+         
+        if(read_size == 0)
+        {
+            puts("Client disconnected");
+            fflush(stdout);
+            close(client_sock);
+        }
+        else if(read_size == -1)
+        {
+            perror("recv failed");
+            close(client_sock);
+        }
+    }
+     
+    return 0;
+}
+
+int main(void)
+{
+    while(1)
+    {
+        loop();
+    }
+}
+
